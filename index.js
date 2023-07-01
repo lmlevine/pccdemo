0f758389eec7d5e80aa4d4e71b214593ab29ec87
Lionel Levine <levine.lionel@gmail.com>
2023-07-01 01:55:15 -0700

    not working but wil push so i can experiment
    this is basically completed demo code

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const mysql = require('mysql')
 const multer = require('multer')
 const path = require('path')
+const csv = require('fast-csv')
+const fs = require('fs')
 
 const app = express()
 
@@ -28,12 +30,37 @@ let upload =  multer({
 })
 
 //Establish connection with MySQL Database
-const pool = mysql.createConnection({
-    host: "127.0.0.1:3306",
+const pool = mysql.createPool({
+    host: "localhost",
     user: "root",
     password: "password",
-    database: "PCC Demo"
+    database: "PCC_Schema"
 })
+
+function uploadCsv(path){
+    let stream = fs.createReadStream(path)
+    let csvData = []
+    let fileStream = csv
+    .parse()
+    .on('data', function(data){
+        csvData.push(data)
+    })
+    .on('end', function(){
+        csvData.shift()
+        pool.getConnection((error,connection)=>{
+            if(error){
+                console.log(error)
+            } else {
+                let query = "INSERT INTO students (LAST_NAME,FIRST_NAME,EMAIL_ADDRESS,PHONE_NUMBER,COURSE_NUMBER,ASSIGNMENT_NUMBER,SUBMISSION_DATE,GRADE) VALUES ?"
+                connection(query,[csvData],(error,res)=>{
+                    console.log(error || res);
+
+                });
+            }
+        } )
+    })
+    stream.pipe(fileStream)
+}
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname + "/index.html")
@@ -42,6 +69,7 @@ app.get('/',(req,res) => {
 
 app.post('/import-csv',upload.single('import-csv'),(req,res) => {
     console.log(req.file.path)
+    uploadCsv(__dirname + "/uploads/" + req.file.filename)
 })
 
 app.listen(5000, () => {
